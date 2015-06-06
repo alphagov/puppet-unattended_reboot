c26c13cd2834fcd7e67dfff414fd58f566d7a005
Matt Bostock <matt@mattbostock.com>
2015-06-06 07:17:42 +0100

    Add manifests, templates and spec tests

---
+++ manifests/init.pp
@@ -0,0 +1,170 @@
+# == Class: unattended_reboot
+#
+# Coordinates unattended reboots of nodes across an environment,
+# using a distributed mutex in etcd.
+#
+# === Parameters
+#
+# [*check_scripts_directory*]
+#   Directory of scripts to verify if it's safe to reboot. A non-zero exit code
+#   from one of these scripts will prevent a reboot.
+#
+#   Default: ''
+#
+# [*cron_month*]
+#   Month of year at which to run unatteded-reboot command.
+#   Default: '*'
+#
+# [*cron_monthday*]
+#   Day of month at which to run unatteded-reboot command.
+#   Default: '*'
+#
+# [*cron_weekday*]
+#   Day of week at which to run unatteded-reboot command.
+#   Default: '*'
+#
+# [*cron_hour*]
+#   Hour of the day at which to run unatteded-reboot command.
+#   Default: '0-5'
+#
+# [*cron_minute*]
+#   Minute of the hour at which to run unatteded-reboot command.
+#   Default: '*/5'
+#
+# [*enabled*]
+#   Whether to enable unattended reboots.
+#   Default: false
+#
+# [*etcd_endpoints*]
+#   An array of etcd client endpoints.
+#
+#   Example: [ 'http://etcd-1.foo:2379', 'http://etcd-2.foo:2379',
+#   'http://etcd-3.foo:2379' ]
+#
+#   Mandatory if enabled is set to `true`; defaults to [
+#   'http://localhost:2379', 'http://localhost:4001' ]
+#
+#   Note: 2379 is IANA-assigned etcd client port, 4001 is the legacy etcd
+#   client port; we try both.
+#
+# [*manage_package*]
+#   Whether this module should manage installation/removal of the
+#   'locksmithctl' package.
+#
+#   Default: true
+#
+# [*pre_reboot_scripts_directory*]
+#   Directory of scripts to run before rebooting a machine to ensure that it
+#   reboots gracefully (e.g. take the node out of a cluster). A non-zero exit
+#   code will abort the reboot.
+#
+#   Default: ''
+#
+# [*run_unattended_upgrade*]
+#   If true, 'unattended-upgrade' will be run within the reboot window defined
+#   by the 'cron_*' parameters to maximise the chance that a package will be
+#   upgraded and trigger a reboot.
+#   Default: false
+#
+class unattended_reboot (
+  $check_scripts_directory = '',
+  $cron_month = '*',
+  $cron_monthday = '*',
+  $cron_weekday = '*',
+  $cron_hour = '0-7',
+  $cron_minute = '*/5',
+  $enabled = false,
+  $etcd_endpoints = [ 'http://localhost:2379', 'http://localhost:4001' ],
+  $manage_package = true,
+  $pre_reboot_scripts_directory = '',
+  $run_unattended_upgrade = false,
+) {
+
+  if $check_scripts_directory != '' {
+    validate_absolute_path($check_scripts_directory)
+  }
+  if $pre_reboot_scripts_directory != '' {
+    validate_absolute_path($pre_reboot_scripts_directory)
+  }
+
+  validate_array($etcd_endpoints)
+  validate_bool($enabled)
+  validate_bool($manage_package)
+  validate_bool($run_unattended_upgrade)
+
+  if ($enabled) {
+    $cron_ensure = present
+    $directory_ensure = directory
+    $file_ensure = present
+    $pkg_ensure = latest
+
+    if $run_unattended_upgrade {
+      $unattended_upgrade_cron_ensure = present
+    } else {
+      $unattended_upgrade_cron_ensure = absent
+    }
+
+    if empty($etcd_endpoints) {
+      fail('Must pass non-empty array to unattended_reboot::etcd_endpoints')
+    }
+  } else {
+    $cron_ensure = absent
+    $directory_ensure = absent
+    $file_ensure = absent
+    $pkg_ensure  = purged
+    $unattended_upgrade_cron_ensure = absent
+  }
+
+  # Upstart script to release reboot lock on boot
+  file { '/etc/init/post-reboot-unlock.conf':
+    ensure  => $file_ensure,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('unattended_reboot/post-reboot-unlock.erb'),
+  }
+
+  if $manage_package {
+    package { 'locksmithctl':
+      ensure => $pkg_ensure,
+      before => File['/etc/init/post-reboot-unlock.conf'],
+    }
+  }
+
+  # Check if a reboot is required and attempt to grab the reboot mutex.
+  cron { 'unattended-reboot':
+    ensure      => $cron_ensure,
+    month       => $cron_month,
+    monthday    => $cron_monthday,
+    weekday     => $cron_weekday,
+    hour        => $cron_hour,
+    minute      => $cron_minute,
+    user        => 'root',
+    environment => ['MAILTO=""'],
+    command     => '/usr/local/bin/unattended-reboot',
+    require     => Package['update-notifier-common'],
+  } ->
+  file { '/usr/local/bin/unattended-reboot':
+    ensure  => $file_ensure,
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    require => Package['locksmithctl'],
+    content => template('unattended_reboot/unattended-reboot.erb'),
+  }
+
+  # Run unattended upgrade to maximise the chance that an upgraded package is
+  # installed within the reboot window
+  cron { 'unattended-upgrade':
+    ensure   => $unattended_upgrade_cron_ensure,
+    month    => $cron_month,
+    monthday => $cron_monthday,
+    weekday  => $cron_weekday,
+    hour     => $cron_hour,
+    minute   => fqdn_rand(59),
+    user     => 'root',
+    command  => '/usr/bin/unattended-upgrade',
+    require  => Package['unattended-upgrades'],
+  }
+
+}
---
+++ spec/classes/unattended_reboot_spec.rb
@@ -0,0 +1,136 @@
+require_relative '../spec_helper'
+
+describe 'unattended_reboot', :type => :class do
+  let(:facts) {{
+    :fqdn => 'foo.example.com',
+  }}
+  let(:default_params) {{
+    :enabled => true,
+  }}
+
+  describe "enabled" do
+    let(:params) { default_params.merge({
+      :etcd_endpoints => ['http://etcd-1.foo:4001', 'http://etcd-2.foo:4001'],
+    }) }
+
+    it { should contain_file('/etc/init/post-reboot-unlock.conf').with_ensure('present') }
+    it { should contain_file('/usr/local/bin/unattended-reboot').with_ensure('present') }
+
+    it { should contain_cron('unattended-reboot').with_ensure('present') }
+
+    it "passes all endpoints to locksmithctl" do
+      should contain_file('/usr/local/bin/unattended-reboot').with_content(/locksmithctl -endpoint='http:\/\/etcd-1\.foo:4001,http:\/\/etcd-2\.foo:4001' lock/)
+    end
+
+    it "uses the correct FQDN when obtaining the reboot mutex" do
+      should contain_file('/usr/local/bin/unattended-reboot').with_content(/locksmithctl.*lock 'foo\.example\.com'/)
+    end
+
+    it "includes all etcd endpoints when unlocking after a reboot" do
+      should contain_file('/etc/init/post-reboot-unlock.conf').with_content(/locksmithctl -endpoint='http:\/\/etcd-1\.foo:4001,http:\/\/etcd-2\.foo:4001' unlock/)
+    end
+
+    context "manage_package is true" do
+      let(:params) { default_params.merge({
+        :manage_package => true,
+      })}
+
+      it { should contain_package('locksmithctl').with_ensure('latest') }
+    end
+
+    context "manage_package is false" do
+      let(:params) { default_params.merge({
+        :manage_package => false,
+      })}
+
+      it { should_not contain_package('locksmithctl') }
+    end
+
+    context "run_unattended_upgrade is true" do
+      let(:params) { default_params.merge({
+        :run_unattended_upgrade => true,
+      })}
+
+      it { should contain_cron('unattended-upgrade').with_ensure('present') }
+    end
+
+    context "run_unattended_upgrade is false" do
+      let(:params) { default_params.merge({
+        :run_unattended_upgrade => false,
+      })}
+
+      it { should contain_cron('unattended-upgrade').with_ensure('absent') }
+    end
+
+    context "empty array passed to etcd_endpoints" do
+      let(:params) { default_params.merge({
+        :etcd_endpoints => [],
+      })}
+
+      it { should raise_error(Puppet::Error, /Must pass non-empty array/) }
+    end
+
+    context "check_scripts_directory set" do
+      let(:params) { default_params.merge({
+        :check_scripts_directory => '/path',
+      })}
+
+      it { should contain_file('/usr/local/bin/unattended-reboot').with_content(/^\/bin\/run-parts --exit-on-error \/path$/) }
+    end
+
+    context "check_scripts_directory not set" do
+      it { should_not raise_error }
+      it { should contain_file('/usr/local/bin/unattended-reboot').without_content(/^\/bin\/run-parts --exit-on-error\s*$/) }
+    end
+
+    context "check_scripts_directory set to non-absolute path" do
+      let(:params) { default_params.merge({
+        :check_scripts_directory => 'relative',
+      })}
+
+      it { should raise_error(Puppet::Error) }
+    end
+
+    context "pre_reboot_scripts_directory set" do
+      let(:params) { default_params.merge({
+        :pre_reboot_scripts_directory => '/path',
+      })}
+
+      it { should contain_file('/usr/local/bin/unattended-reboot').with_content(/^\/bin\/run-parts --exit-on-error \/path$/) }
+    end
+
+    context "pre_reboot_scripts_directory not set" do
+      it { should_not raise_error }
+      it { should contain_file('/usr/local/bin/unattended-reboot').without_content(/^\/bin\/run-parts --exit-on-error\s*$/) }
+    end
+
+    context "pre_reboot_scripts_directory set to non-absolute path" do
+      let(:params) { default_params.merge({
+        :pre_reboot_scripts_directory => 'relative',
+      })}
+
+      it { should raise_error(Puppet::Error) }
+    end
+  end
+
+  describe "disabled" do
+    let(:params) {{
+      :enabled => false,
+    }}
+
+    it { should contain_file('/etc/init/post-reboot-unlock.conf').with_ensure('absent') }
+    it { should contain_file('/usr/local/bin/unattended-reboot').with_ensure('absent') }
+
+    it { should contain_cron('unattended-reboot').with_ensure('absent') }
+    it { should contain_cron('unattended-upgrade').with_ensure('absent') }
+
+    context "disabled and empty array passed to etcd_endpoints" do
+      let(:params) {{
+        :enabled => false,
+        :etcd_endpoints => [],
+      }}
+
+      it { should_not raise_error }
+    end
+  end
+end
